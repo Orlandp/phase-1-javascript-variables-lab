d8c490974b4c9e416644f3da6d9aaed14dfd0cde
pknowledge <jamesblssings@gmail.com>
2024-09-25 16:10:26 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+// Define companyName as 'Scuber' using const
+const companyName = 'Scuber';
+
+// Define mostProfitableNeighborhood as 'Chelsea' using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define companyCeo as 'Susan Smith' using let
+let companyCeo = 'Susan Smith';
